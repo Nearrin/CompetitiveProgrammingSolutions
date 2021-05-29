1f7f37218ae38e2bd317e4bf8f41cdc3c2649a64
Njord Yu <yunjord@gmail.com>
2021-05-29 11:43:54 -0400

    Updated.

--- update.sh
+++ update.sh
@@ -7,6 +7,6 @@ echo "|Total|"$(ls -d */* | grep '\.' | wc -l)"|" | sed s@' '@@g >>README.md
 echo "\n|Language|Number|\n|---|---|" >>README.md
 ls -d */* | grep '\.' | cut -d. -f2 | sort | uniq -c | sort -k1nr | awk '$2=="cpp"{$2="CPP"}$2=="py"{$2="Python"}$2=="sql"{$2="SQL"}$2=="sh"{$2="Linux Shell"}$2=="go"{$2="Go"}{print "|"$2"|"$1"|"}' >>README.md
 echo "|Total|"$(ls -d */* | grep '\.' | wc -l)"|" | sed s@' '@@g >>README.md
-git add *
+git add -A
 git commit -m 'Updated.'
 git push
