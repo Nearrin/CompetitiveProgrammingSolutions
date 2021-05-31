964bc34bdf7d7cb8844bf9ece0192a4229e899cc
Njord Yu <yunjord@gmail.com>
2021-05-31 01:35:55 -0400

    Updated.

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 cd $(git rev-parse --show-toplevel)
-echo "# Number of Problems Solved\nThe table below is a rough estimate based on the code I have saved from multiple accounts I have." >README.md
+echo "# Number of Problems Solved\nThe table below is a rough estimate based on the code I have saved from multiple accounts I have. Some pieces of code are probably written back in 2012, when things are pretty different. I am never really great at competitive programming. The best I've ever got is a bronze medal in National Olympiad in Informatics, China, but it's pretty much similar to a souls-like game, where you suffer, learn and enjoy." >README.md
 echo "|Platform|Number|\n|---|---|" >>README.md
 ls -d */* | grep '\.' | cut -d/ -f1 | sort | uniq -c | sort -k1nr | awk '{c=$1;$1="";print "|"substr($0,2)"|"c"|"}' >>README.md
 echo "|Total|"$(ls -d */* | grep '\.' | wc -l)"|" | sed s@' '@@g >>README.md
